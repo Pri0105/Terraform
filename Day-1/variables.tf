035b90c64325f542765f8e81513fa5ae7d7f2dbb
PRIYA DAS <86485970+Pri0105@users.noreply.github.com>
2023-10-19 15:45:04 +0530

    Update variables.tf

--- Day-1/variables.tf
+++ Day-1/variables.tf
@@ -1,1 +1,15 @@
+variable "ami_value" {
+  description = "This is the value of ami"
 
+}
+
+variable "instance_type_value" {
+    description = "This is the Instance type value"
+  
+}
+
+variable "key_name_value" {
+    description = "Value for key pair"
+
+  
+}
