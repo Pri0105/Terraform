e48b58c5285c22cfb17e334a5c4aa2445053a755
PRIYA DAS <86485970+Pri0105@users.noreply.github.com>
2023-10-19 15:40:48 +0530

    Update main.tf

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+    region = "eu-north-1"  # Set your desired AWS region
+}
 
+resource "aws_instance" "example" {
+    ami = "ami-0ea7dc624e77a15d5"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+    key_name = "mynewkey"
+}
